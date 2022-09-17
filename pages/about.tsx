bd37104f1f406fec56bf36e2c5dcbf2e9d735154
thapaRoyal <thaparoyal17@gmail.com>
2022-09-17 02:36:38 -0400

    starting...

---
+++ pages/about.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const about = () => {
+  return <div>About Page</div>;
+};
+
+export default about;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">.blog</a>
         </h1>
 
         <p className={styles.description}>
@@ -66,7 +66,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
