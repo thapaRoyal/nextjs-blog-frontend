c9d89f33a2dd834e6e85c37c6c270699afd36c8d
thapaRoyal <thaparoyal17@gmail.com>
2022-09-17 05:41:39 -0400

    Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,49 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
-  return <nav>navbar</nav>;
+  return (
+    <nav className="flex items-center justify-between py-6">
+      <Link href="/">
+        <div className="flex items-center cursor-pointer">
+          <Image src="/logo.png" alt=".blog image" height={35} width={40} />
+          <span className="font-bold ml-2 text-primary">.blog</span>
+        </div>
+      </Link>
+
+      <ul className="flex items-center">
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#">Products</a>
+        </li>
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#">Pricing</a>
+        </li>
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#">Docs</a>
+        </li>
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#">Company</a>
+        </li>
+      </ul>
+
+      <ul className="flex items-center">
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#" className="hover:text-gray-400 transition-all">
+            Log in
+          </a>
+        </li>
+        <li className="mr-6 font-medium text-gray-600">
+          <a
+            href="#"
+            className="bg-primary py-2 px-4 rounded-sm text-white hover:bg-primary-dark transition-all"
+          >
+            Sign up
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
