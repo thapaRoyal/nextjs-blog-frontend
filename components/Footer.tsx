6546758ed3029e9bb1612f0cb0796ed93c85375b
thapaRoyal <thaparoyal17@gmail.com>
2022-09-18 10:22:24 -0400

    link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © 2022 .blog —
           <a
-            href="https://twitter.com/codersgyan"
+            href="https://twitter.com/thaparoyal17"
             className="text-gray-600 ml-1"
             rel="noopener noreferrer"
             target="_blank"
