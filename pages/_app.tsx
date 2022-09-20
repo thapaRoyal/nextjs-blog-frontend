80a2a50c54fedc251f05bcf898ff41ba2e25b127
thapaRoyal <thaparoyal17@gmail.com>
2022-09-20 06:26:06 -0400

    progessbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <div className="container mx-auto font-sans">
+        <NextNProgress color="#53bd95" />
         <Navbar />
         <main className="pb-32">
           <Component {...pageProps} />
